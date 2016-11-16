060c67e5ba37615f890f5f980babd960400ba2fc
Chaiyong Ragkhitwetsagul <cragkhit@gmail.com>
2016-11-16 22:06:04 +0000

    Update

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,8 +4,8 @@
 public class Test {
     public static void main(String[] args) {
         // String s = "         result = prime * result + ( ( id == null ) ? 0 : id.hashCode() );";
-        String s = "         return id;";
+        String s = "     public int hashCode() {";
         // System.out.println(s.matches("\\s*result = prime \\* result \\+ ( ( id == null ) \\? 0 : id\\.hashCode() );"));
-        System.out.println(s.matches("\\s*return\\s*[a-zA-Z0-9]*;"));
+        System.out.println(s.matches("\\s*public\\s*int\\s*hashCode\\(\\)\\s*\\{\\s*"));
     }
 }
