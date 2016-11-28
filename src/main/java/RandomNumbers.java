021114331ed22cb720b8e8718f33443d0c6e74eb
Chaiyong Ragkhitwetsagul <ucabagk@ucl.ac.uk>
2016-11-28 00:44:57 +0000

    Added code to sample for manual investigation

---
+++ src/main/java/RandomNumbers.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+
+/**
+ * Created by Chaiyong on 11/28/16.
+ */
+public class RandomNumbers {
+    public static void main(String[] args) {
+        int somePredefinedSeed = 53;
+        int sampleSize = 26;
+
+        Integer[] arr = new Integer[523];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+        Collections.shuffle(Arrays.asList(arr), new Random(somePredefinedSeed));
+        System.out.println(Arrays.toString(arr));
+        System.out.println("SAMPLED:");
+
+        for (int i = 0; i < sampleSize; i++) {
+            if (i == sampleSize - 1)
+                System.out.print(arr[i]);
+            else
+                System.out.print(arr[i] + ", ");
+        }
+
+    }
+}
